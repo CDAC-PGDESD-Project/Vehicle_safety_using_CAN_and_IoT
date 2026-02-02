aaa842aac44a8477d9929e9c885ac0a29b273d0a
KrushnaKapare <krushnakapare25@gmail.com>
2026-02-02 15:41:10 +0530

    CAN TX

--- HAL_CAN_TX/Core/Inc/stm32f4xx_it.h
+++ HAL_CAN_TX/Core/Inc/stm32f4xx_it.h
@@ -57,6 +57,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void CAN1_RX0_IRQHandler(void);
 void TIM4_IRQHandler(void);
+void DMA2_Stream2_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
