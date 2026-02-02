3f026fdd03f783ddc2a181c875b0535fed14b779
KrushnaKapare <krushnakapare25@gmail.com>
2026-02-02 18:35:54 +0530

    CAN TX

--- HAL_CAN_TX/Core/Src/main.c
+++ HAL_CAN_TX/Core/Src/main.c
@@ -50,8 +50,6 @@ DMA_HandleTypeDef hdma_adc2;
 
 CAN_HandleTypeDef hcan1;
 
-I2C_HandleTypeDef hi2c1;
-
 TIM_HandleTypeDef htim4;
 
 UART_HandleTypeDef huart2;
@@ -79,7 +77,6 @@ static void MX_CAN1_Init(void);
 static void MX_TIM4_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_ADC2_Init(void);
-static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -195,7 +192,6 @@ int main(void)
   MX_TIM4_Init();
   MX_USART2_UART_Init();
   MX_ADC2_Init();
-  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   HAL_ADC_Start_DMA(&hadc2, (uint32_t*)&adc_dma_value, 1);
    HAL_CAN_Start(&hcan1);
@@ -375,40 +371,6 @@ static void MX_CAN1_Init(void)
   if(HAL_CAN_ConfigFilter(&hcan1, &FilterConfig) != HAL_OK)
 	   Error_Handler();
   /* USER CODE END CAN1_Init 2 */
-
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
 
 }
 
